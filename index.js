7632b5333a1aa2ff419803d8c2c0c6613cbcaee3
Kevin Marks <kevin.marks.ext@bbc.co.uk>
2019-03-27 18:23:25 +0000

    Add Norway option
    https://petition.parliament.uk/petitions/234587

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ nunjucks.configure(path.resolve(__dirname, 'views'), {
     autoescape: true,
     express: app
 });
-const petitions={241584:"Revoke A50",229963:"No Deal",243319:"Leave or No deal",235138:"2nd Referendum",232984:"Vote if Rejected"}
-const content={"petitions":[
-    {"name":"Revoke Article 50 and remain in the EU.","summary":"The government repeatedly claims exiting the EU is 'the will of the people'. We need to put a stop to this claim by proving the strength of public support now, for remaining in the EU. A People's Vote may not happen - so vote now.",
-     "count":5758090,"id":241584},
-    {"name":"Leave the EU without a deal in March 2019.","summary":"We are wasting Billions of pounds of taxpayers money trying to negotiate in a short space of time. Leaving the EU in March 2019 will allow the UK good time to negotiate more efficiently. The EU will be more eager to accept a deal on our terms having lost a major partner.",
-     "count":571441,"id":229963},
-]};
+const petitions={241584:"Revoke A50",229963:"No Deal",243319:"Leave or No deal",235138:"2nd Referendum",232984:"Vote if Rejected",234587:"Norway EEA/EFTA"}
 function get_petitions(id_list){
     let op={"err":""};
     let max=0;
