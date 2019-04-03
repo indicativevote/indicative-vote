63da4225fa6c55acdbca8ffaa4bbfed16edb393f
Kevin Marks <kevin.marks.ext@bbc.co.uk>
2019-04-03 14:52:32 +0100

    Add freedom of movement and enquiry

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const petitions={241584:"Revoke Article 50 and Remain",229963:"No Deal in March"
     235138:"2nd Referendum",232984:"Vote if Rejected",234587:"Norway EEA/EFTA",
     233483:"Withdraw from Backstop",233767:"Schengen and Euro",239706:"Revoke if no plan",
     235954:"Voting for EU in UK & UK in EU",248281:"No Deal April 12 ",
-    233104:"PM May’s Deal",
+    233104:"PM May’s Deal",229678:"Keep Freedom of Movement",241848:"Halt Brexit for Public Enquiry",
     //254716:"UK vote in EU Elections" https://petition.parliament.uk/petitions/254716/sponsors/new?token=q0Y4VfI8JZgeGtjThn
     }
 function get_petitions(id_list){
